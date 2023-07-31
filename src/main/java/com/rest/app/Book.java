75784cfa7cd54d5fb243cf894210eaa3dbc6c6c3
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-07-31 14:35:20 +0100

    entity, repository & controller created

---
+++ src/main/java/com/rest/app/Book.java
@@ -0,0 +1,26 @@
+package com.rest.app;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book_record")
+@Data
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Builder
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bookId;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String summary;
+
+    private int rating;
+}
