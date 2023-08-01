b441ad5f381ce48b1cdfb1775ed9f88f032a21c7
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-08-01 07:32:17 +0100

    controller methods

--- src/main/java/com/rest/app/BookController.java
+++ src/main/java/com/rest/app/BookController.java
@@ -1,8 +1,12 @@
 package com.rest.app;
 
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/book")
@@ -10,4 +14,19 @@ public class BookController {
 
     @Autowired
     BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Book getBookById(@PathVariable (value = "id") Long bookId) {
+        return bookRepository.findById(bookId).get();
+    }
+
+    @PostMapping
+    public Book createBookRecord(@RequestBody @Valid Book bookRecord) {
+        return bookRepository.save(bookRecord);
+    }
 }
